13ef8e49d8d71f81f4d18d29ad7a1ab0c5617b89
Tom Ward <tomward@fmail.co.uk>
2020-08-23 00:21:37 +0100

    Don't use panic! when user doesn't specify all required cmdline args

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,23 @@ mod jack_trip_header;
 mod jack_notifications;
 mod potential_chainsaw;
 
+fn print_usage_information() {
+  println!("Please specify `-c IPADDRESS` or `-s`");
+}
+
 fn main() {
   let args: Vec<String> = env::args().collect();
-  // println!("{:?}", args);
+  if args.len() < 2 {
+    print_usage_information();
+    return;
+  }
 
   let debug_mode = false;
-
-  if args.len() < 2 {
-    panic!("Please specify `-c IPADDRESS` or `-s`");
-  }
   if args[1] == "-c" {
     potential_chainsaw::jacktrip_connect(debug_mode, true, args[2].clone());
   }
   if args[1] == "-s" {
     potential_chainsaw::jacktrip_connect(debug_mode, false, "".to_string());
   }
-  panic!("Please specify server or client");
+  print_usage_information();
 }
